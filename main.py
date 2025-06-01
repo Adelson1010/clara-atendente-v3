e9ef5ab1e02af2d86b8f417a457e07467490f9b0
Adelson1010 <senhoradelsonsousa@gmail.com>
2025-06-01 13:48:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
-def webhook():
-    if request.method == "POST":
-        data = request.json
-        print("📩 Webhook recebido:")
-        print(data)
-        return "Recebido com sucesso", 200
+@app.route("/", methods=["POST"])
+def receber_webhook():
+    data = request.json
+    print("📩 Webhook recebido:", data)
+    return "OK", 200
+
+@app.route("/", methods=["GET"])
+def home():
     return "Clara está online!", 200
 
 if __name__ == "__main__":
